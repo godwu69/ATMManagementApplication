03e12a2dd03ab593c2a72c6fbdf6915c4f61aeb6
sukablyat <godwu69@gmail.com>
2024-11-18 22:53:52 +0700

    update

--- obj/Debug/net8.0/ATMManagementApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/ATMManagementApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATMManagementApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cc5777e8ea74986e64b6e1c19c88c828a45ade9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da565be1cc737a88696660b2fb331ade65247cc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATMManagementApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATMManagementApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
